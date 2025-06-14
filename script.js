d5e900041d94e65780f0059db91b7b19479e4e18
iWanah02 <uzumakihimawari25@gmail.com>
2025-06-14 19:10:50 +0800

    commit

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const flavors = [
-  { id: 1, name: "Vanilla", price: 60, image: "https://via.placeholder.com/100?text=Vanilla" },
-  { id: 2, name: "Chocolate", price: 70, image: "https://via.placeholder.com/100?text=Choco" },
-  { id: 3, name: "Strawberry", price: 65, image: "https://via.placeholder.com/100?text=Strawberry" },
-  { id: 4, name: "Mango", price: 75, image: "https://via.placeholder.com/100?text=Mango" }
+const book = [
+  { id: 1, name: "Arkitek Pelukis Jalanan", price: 35, image: "arkitekjalanan.jpg" },
+  { id: 2, name: "Alexander Adalah Zulkarnain", price: 150, image: "alexanderzulkarnain.jpg" },
+  { id: 3, name: "Kids Book", price: 30, image: "kidsbook.jpg" },
+  { id: 4, name: "Tamadun Mesir Purba", price: 80, image: "tamadunmesir.jpg" }
 ];
 
 const cart = {};
-const flavorList = document.getElementById("flavor-list");
+const bookList = document.getElementById("book-list");
 const orderItems = document.getElementById("order-items");
 const totalDisplay = document.getElementById("total");
 
-// Render flavors
-flavors.forEach(flavor => {
+// Render books
+book.forEach(book => {
   const div = document.createElement("div");
-  div.className = "flavor";
+  div.className = "book";
   div.innerHTML = `
-    <img src="${flavor.image}" alt="${flavor.name}"/>
-    <h3>${flavor.name}</h3>
-    <p>₹${flavor.price}</p>
-    <button onclick="addToOrder(${flavor.id})">Add to Order</button>
+    <img src="${book.image}" alt="${book.name}"/>
+    <h3>${book.name}</h3>
+    <p>RM${book.price}</p>
+    <button onclick="addToOrder(${book.id})">Add to Cart</button>
   `;
-  flavorList.appendChild(div);
+  bookList.appendChild(div);
 });
 
 function addToOrder(id) {
   if (cart[id]) {
     cart[id].qty += 1;
   } else {
-    const item = flavors.find(f => f.id === id);
+    const item = book.find(f => f.id === id);
     cart[id] = { ...item, qty: 1 };
   }
   renderOrder();
@@ -40,7 +40,7 @@ function renderOrder() {
     total += item.price * item.qty;
     orderItems.innerHTML += `
       <div>
-        ${item.name} - ₹${item.price} x ${item.qty}
+        ${item.name} - RM${item.price} x ${item.qty}
         <button onclick="changeQty(${item.id}, 1)">+</button>
         <button onclick="changeQty(${item.id}, -1)">-</button>
         <button onclick="removeItem(${item.id})">Remove</button>
